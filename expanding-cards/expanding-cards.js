3560adc7db0ff90b29b6ac450efc37604a948aff
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-11 10:12:14 -0300

    feat: Atualização no layout dos cards e criação da função para expandir os cards.

---
+++ expanding-cards/expanding-cards.js
@@ -0,0 +1,14 @@
+const card = document.querySelectorAll(".card");
+
+card.forEach((card) => card.addEventListener('click', () => {
+
+    removeClassActive()
+    card.classList.add("active")
+
+}))
+
+function removeClassActive() {
+    card.forEach((card) => {
+        card.classList.remove("active")
+    })
+}
