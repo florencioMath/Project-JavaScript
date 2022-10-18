e4119e261454848c66d7574e11acff968d3621ac
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-18 11:16:46 -0300

    feat: Conclusão do desafio Form Input Wave.

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -3,26 +3,37 @@ const labelPassword = document.getElementById("labelPassword");
 const inputName = document.getElementById("inputName");
 const inputPassword = document.getElementById("inputPassword");
 
-inputName.addEventListener("focus", (event) => {
-  labelName.classList.add("formWave");
+const labels = document.querySelectorAll(".containerLabelInput label");
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 80}ms">${letter}</span>`
+    )
+    .join("");
 });
 
-inputName.addEventListener("focusout", (event) => {
-  labelName.classList.remove("formWave");
-});
+// inputName.addEventListener("focus", () => {
+//   labelName.classList.add("formWave");
+// });
 
-inputPassword.addEventListener("focus", (event) => {
-  labelPassword.classList.add("formWave");
-});
+// // inputName.addEventListener("focusout", () => {
+// //   labelName.classList.remove("formWave");
+// // });
 
-inputPassword.addEventListener("focusout", (event) => {
-  labelPassword.classList.remove("formWave");
-});
+// inputPassword.addEventListener("focus", () => {
+//   labelPassword.classList.add("formWave");
+// });
 
-labelName.addEventListener("click", (event) => {
-  inputName.focus();
-});
+// inputPassword.addEventListener("focusout", () => {
+//   labelPassword.classList.remove("formWave");
+// });
 
-labelPassword.addEventListener("click", (event) => {
-  inputPassword.focus();
-});
+// labelName.addEventListener("click", () => {
+//   inputName.focus();
+// });
+
+// labelPassword.addEventListener("click", () => {
+//   inputPassword.focus();
+// });
