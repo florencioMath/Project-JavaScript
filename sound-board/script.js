c0fe23051d7b04c6bccf9c60d1ce0688d8412511
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-19 11:16:49 -0300

    feat: Desafio Sound Board com algumas melhorias no código e Layout.

--- sound-board/script.js
+++ sound-board/script.js
@@ -1,24 +1,47 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+function soundBoard() {
+  const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-sounds.forEach((sound) => {
-  const btn = document.createElement("button");
-  btn.classList.add("buttonCreated");
-  btn.innerText = sound.charAt(0).toUpperCase() + sound.slice(1);
-
-  btn.addEventListener("click", () => {
-    songStop();
-    document.getElementById(sound).play();
-  });
-
-  document.getElementById("buttons").appendChild(btn);
-});
-
-function songStop() {
   sounds.forEach((sound) => {
-    const song = document.getElementById(sound);
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+    wrapper.id = "wrapper";
 
-    song.pause();
+    const song = document.createElement("div");
+    song.classList.add("buttonCreated");
+    song.innerText = sound.charAt(0).toUpperCase() + sound.slice(1);
 
-    song.currentTime = 0;
+    const btnPlay = document.createElement("button");
+    btnPlay.classList.add("btnPlay");
+    btnPlay.id = "btnPlay";
+
+    const btnStop = document.createElement("button");
+    btnStop.classList.add("btnStop");
+
+    btnPlay.addEventListener("click", () => {
+      document.getElementById(sound).play();
+
+      btnPlay.style.display = "none";
+      btnStop.style.display = "flex";
+    });
+
+    btnStop.addEventListener("click", () => {
+      songStop();
+      btnPlay.style.display = "flex";
+      btnStop.style.display = "none";
+    });
+
+    document.getElementById("buttons").appendChild(wrapper);
+    wrapper.appendChild(btnPlay);
+    wrapper.appendChild(btnStop);
+    wrapper.appendChild(song);
   });
+
+  function songStop() {
+    sounds.forEach((sound) => {
+      const song = document.getElementById(sound);
+      song.pause();
+    });
+  }
 }
+
+soundBoard();
