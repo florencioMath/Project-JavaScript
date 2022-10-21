0d7aadf6efad9d630dd9e8c6960c91e1a319422a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-21 15:50:42 -0300

    feat: Conclusão do desafio Random Choice picker

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,76 +1,96 @@
-const textarea = document.getElementById("textarea");
-const tagsField = document.getElementById("tagsField");
-const btnChoicePicker = document.getElementById("btnChoicePicker");
+function randomChoicePicker() {
+  const textarea = document.getElementById("textarea");
+  const tagsField = document.getElementById("tagsField");
+  const btnChoicePicker = document.getElementById("btnChoicePicker");
+  const btnChoicePickerClear = document.getElementById("btnChoicePickerClear");
 
-textarea.focus();
+  textarea.focus();
 
-textarea.addEventListener("keyup", (event) => {
-  creatTags(event.target.value);
+  textarea.addEventListener("keyup", (event) => {
+    creatTags(event.target.value);
 
-  pickRandomTag();
-});
+    pickRandomTag();
+    if (textarea.value.length > 0) {
+      btnChoicePickerClear.disabled = false;
+    }
+  });
 
-btnChoicePicker.addEventListener("click", (e) => {
-  e.preventDefault(e);
-  randomSelect();
-});
-
-function creatTags(input) {
-  const tags = input
-    .split(",")
-    .map((string) => string.trim())
-    .filter((string) => string !== "");
-
-  if (tags.length > 0) {
-    btnChoicePicker.disabled = false;
-    tagsField.innerHTML = "";
-  } else {
+  btnChoicePickerClear.addEventListener("click", (e) => {
+    e.preventDefault(e);
+    textarea.value = "";
     tagsField.innerHTML = "Your tags will show here.";
+    btnChoicePickerClear.disabled = true;
+    btnChoicePicker.disabled = true;
+  });
+
+  btnChoicePicker.addEventListener("click", (e) => {
+    e.preventDefault(e);
+    setTimeout(() => {
+      textarea.value = "";
+    }, 100);
+
+    randomSelect();
+  });
+
+  function creatTags(input) {
+    const tags = input
+      .split(",")
+      .map((string) => string.trim())
+      .filter((string) => string !== "");
+
+    if (tags.length > 0) {
+      btnChoicePicker.disabled = false;
+      tagsField.innerHTML = "";
+    } else {
+      tagsField.innerHTML = "Your tags will show here.";
+    }
+
+    tags.forEach((tag) => {
+      const span = document.createElement("span");
+      span.classList.add("span");
+      span.innerHTML = tag;
+      tagsField.appendChild(span);
+    });
   }
 
-  tags.forEach((tag) => {
-    const span = document.createElement("span");
-    span.classList.add("span");
-    span.innerHTML = tag;
-    tagsField.appendChild(span);
-  });
-}
+  function randomSelect() {
+    const times = 50;
 
-function randomSelect() {
-  const times = 50;
-
-  const interval = setInterval(() => {
-    const randomTag = pickRandomTag();
-
-    if (randomTag !== undefined) {
-      highlightTag(randomTag);
-
-      setTimeout(() => {
-        unHighlightTag(randomTag);
-      }, 100);
-    }
-  }, 100);
-
-  setTimeout(() => {
-    clearInterval(interval);
-
-    setTimeout(() => {
+    const interval = setInterval(() => {
       const randomTag = pickRandomTag();
 
-      highlightTag(randomTag);
+      if (randomTag !== undefined) {
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+          unHighlightTag(randomTag);
+        }, 100);
+      }
     }, 100);
-  }, times * 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+
+      setTimeout(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+      }, 100);
+    }, times * 100);
+  }
+
+  function pickRandomTag() {
+    const tags = document.querySelectorAll(".span");
+    return tags[Math.floor(Math.random() * tags.length)];
+  }
+
+  function highlightTag(tag) {
+    tag.classList.add("highlight");
+  }
+
+  function unHighlightTag(tag) {
+    tag.classList.remove("highlight");
+  }
 }
 
-function pickRandomTag() {
-  const tags = document.querySelectorAll(".span");
-  return tags[Math.floor(Math.random() * tags.length)];
-}
-
-function highlightTag(tag) {
-  tag.classList.add("highlight");
-}
-
-function unHighlightTag(tag) {
-  tag.classList.remove("highlight");
-}
+randomChoicePicker();
