a04235737a6e0851f6c9e048af3f34dcc6cf0046
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-21 15:04:00 -0300

    feat: Implementação da função do random e estilos para o selecionado.

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,25 +1,76 @@
-const textarea = document.getElementById("choices");
-const tags = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+const tagsField = document.getElementById("tagsField");
+const btnChoicePicker = document.getElementById("btnChoicePicker");
 
-let tagsArray = [];
+textarea.focus();
 
-textarea.addEventListener("input", (event) => {
-  console.log(
-    event.target.value
-      .split(",")
-      .map((string) => string.trim())
-      .filter((string) => string !== ",")
-  );
+textarea.addEventListener("keyup", (event) => {
+  creatTags(event.target.value);
 
-  tags.innerText = event.target.value.split(",").map((string) => string.trim());
-
-  //   let text = textarea.value.split(",").toString().trim();
-  //   //   console.log(event.target.value);
-  //   //   tagsArray = text.trim();
-  //   tagsArray = [text.trim()];
-  //   console.log("textare", textarea.value.split(",").toString().trim());
-  //   console.log("tagsArray", tagsArray);
-  //   tags.innerHTML = tagsArray;
+  pickRandomTag();
 });
 
-// console.log(tagsArray);
+btnChoicePicker.addEventListener("click", (e) => {
+  e.preventDefault(e);
+  randomSelect();
+});
+
+function creatTags(input) {
+  const tags = input
+    .split(",")
+    .map((string) => string.trim())
+    .filter((string) => string !== "");
+
+  if (tags.length > 0) {
+    btnChoicePicker.disabled = false;
+    tagsField.innerHTML = "";
+  } else {
+    tagsField.innerHTML = "Your tags will show here.";
+  }
+
+  tags.forEach((tag) => {
+    const span = document.createElement("span");
+    span.classList.add("span");
+    span.innerHTML = tag;
+    tagsField.appendChild(span);
+  });
+}
+
+function randomSelect() {
+  const times = 50;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if (randomTag !== undefined) {
+      highlightTag(randomTag);
+
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".span");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
