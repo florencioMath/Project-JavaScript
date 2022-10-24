dae2ecb75f601e10869645fc3cb959e76e029693
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-24 13:33:10 -0300

    feat: Conclusão do desafio Drink Water.

--- drink-water/script.js
+++ drink-water/script.js
@@ -1,52 +1,57 @@
-const cupSmall = document.querySelectorAll(".cupSmall");
-const liters = document.getElementById("liters");
-const percentage = document.getElementById("percentage");
-const remaining = document.getElementById("remaining");
-
-fullCup();
-
-cupSmall.forEach((cup, index) => {
-  cup.addEventListener("click", () => drinkWater(index));
-});
-
-function drinkWater(index) {
-  if (index === 7 && cupSmall[index].classList.contains("full")) index--;
-  else if (
-    cupSmall[index].classList.contains("full") &&
-    !cupSmall[index].nextElementSibling.classList.contains("full")
-  ) {
-    index--;
-  }
-
-  cupSmall.forEach((cup2, index2) => {
-    if (index2 <= index) {
-      cup2.classList.add("full");
-    } else {
-      cup2.classList.remove("full");
-    }
-  });
+function drinkWater() {
+  const cupSmall = document.querySelectorAll(".cupSmall");
+  const liters = document.getElementById("liters");
+  const percentage = document.getElementById("percentage");
+  const remaining = document.getElementById("remaining");
 
   fullCup();
-}
 
-function fullCup() {
-  const totalSmallCupsFull = document.querySelectorAll(".cupSmall.full").length;
-  const totalCups = cupSmall.length;
+  cupSmall.forEach((cup, index) => {
+    cup.addEventListener("click", () => drink(index));
+  });
 
-  if (totalSmallCupsFull === 0) {
-    percentage.style.visibility = "hidden";
-    percentage.style.height = 0;
-  } else {
-    percentage.style.visibility = "visible";
-    percentage.style.height = `${(totalSmallCupsFull / totalCups) * 400}px`;
-    percentage.innerText = `${(totalSmallCupsFull / totalCups) * 100}%`;
+  function drink(index) {
+    if (index === 7 && cupSmall[index].classList.contains("full")) index--;
+    else if (
+      cupSmall[index].classList.contains("full") &&
+      !cupSmall[index].nextElementSibling.classList.contains("full")
+    ) {
+      index--;
+    }
+
+    cupSmall.forEach((cup2, index2) => {
+      if (index2 <= index) {
+        cup2.classList.add("full");
+      } else {
+        cup2.classList.remove("full");
+      }
+    });
+
+    fullCup();
   }
 
-  if (totalSmallCupsFull === totalCups) {
-    remaining.style.visibility = "hidden";
-    remaining.style.height = 0;
-  } else {
-    remaining.style.visibility = "visible";
-    liters.innerText = `${2 - (250 * totalSmallCupsFull) / 1000}L`;
+  function fullCup() {
+    const totalSmallCupsFull =
+      document.querySelectorAll(".cupSmall.full").length;
+    const totalCups = cupSmall.length;
+
+    if (totalSmallCupsFull === 0) {
+      percentage.style.visibility = "hidden";
+      percentage.style.height = 0;
+    } else {
+      percentage.style.visibility = "visible";
+      percentage.style.height = `${(totalSmallCupsFull / totalCups) * 350}px`;
+      percentage.innerText = `${(totalSmallCupsFull / totalCups) * 100}%`;
+    }
+
+    if (totalSmallCupsFull === totalCups) {
+      remaining.style.visibility = "hidden";
+      remaining.style.height = 0;
+    } else {
+      remaining.style.visibility = "visible";
+      liters.innerText = `${2 - (250 * totalSmallCupsFull) / 1000}L`;
+    }
   }
 }
+
+drinkWater();
