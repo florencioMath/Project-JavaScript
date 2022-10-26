34d672295b3ec20728ce4510c511d57ebceba7ec
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-10-26 11:42:14 -0300

    feat: Criado um header com campo para pesquisar expansível.

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,14 +1,46 @@
-const api_key = "api_key=934ac50f08c8f20204bbaf41bc00f59e";
-const base_uri = "http://api.themoviedb.org/3/";
-const final_url =
-  base_uri + "/discover/movie?sort_by=popularity.desc&" + api_key;
-const images_uri = "http://image.tmdb.org/t/p/";
+const API_KEY = "api_key=934ac50f08c8f20204bbaf41bc00f59e";
+const BASE_URL = "http://api.themoviedb.org/3/";
+const FINAL_URL =
+  BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMAGES_URL = "http://image.tmdb.org/t/p/";
+const SEARCH_API = BASE_URL + "search/movie?" + API_KEY + "&query=";
 const timeout = 2000;
 
+const btnSearch = document.getElementById("btnSearch");
+const inputSearch = document.getElementById("inputSearch");
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault(e);
+
+  if (inputSearch.classList.contains("inputSearchExpand")) {
+    inputSearch.classList.remove("inputSearchExpand");
+    btnSearch.classList.remove("btnSearchClick");
+  } else {
+    inputSearch.classList.add("inputSearchExpand");
+    btnSearch.classList.add("btnSearchClick");
+    inputSearch.focus();
+  }
+});
+
+inputSearch.addEventListener("keyup", ({ target }) => {
+  const movieToSearch = target.value;
+  if (movieToSearch.length > 0) {
+    getMoviesSearch(movieToSearch);
+  }
+});
+
 async function getMovies() {
-  const response = await fetch(final_url);
+  const response = await fetch(FINAL_URL);
+  const data = await response.json();
+  // console.log(data);
+}
+
+getMovies();
+
+async function getMoviesSearch(movie) {
+  const response = await fetch(SEARCH_API + movie);
   const data = await response.json();
   console.log(data);
 }
 
-getMovies();
+// getMoviesSearch("Halloween");
