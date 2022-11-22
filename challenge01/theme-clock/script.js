b2b9a43a76c6d25fa35257f1095eed8557397185
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-11-22 20:32:15 -0300

    feat: Reorganização do script e movimentação dos ponteiros.

--- challenge01/theme-clock/script.js
+++ challenge01/theme-clock/script.js
@@ -1,15 +1,3 @@
-const date = new Date();
-const [day, month, year] = [
-  date.getUTCDate(),
-  date.getUTCMonth() + 1,
-  date.getUTCFullYear(),
-];
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
-
 const weeksDays = [
   "Sunday",
   "Monday",
@@ -34,15 +22,41 @@ const yearMonths = [
   "December",
 ];
 
-const thisMonth = yearMonths[date.getMonth()];
-const thisWeekDay = weeksDays[date.getDay()];
-const digitalTime = document.getElementById("digitalTime");
-const dateTime = document.getElementById("dateTime");
-const meridiemTime = hour <= 12 ? "AM" : "PM";
-const dateDigitalTime = `${hour}:${
-  minutes <= 9 ? "0" + minutes : minutes
-} ${meridiemTime} `;
-const dateTimeBody = `${thisWeekDay}, ${thisMonth} ${day}`;
+setInterval(() => {
+  const date = new Date();
+  const [day, month, year] = [
+    date.getUTCDate(),
+    date.getUTCMonth() + 1,
+    date.getUTCFullYear(),
+  ];
+  const [hour, minutes, seconds] = [
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+  ];
+  const thisMonth = yearMonths[date.getMonth()];
+  const thisWeekDay = weeksDays[date.getDay()];
+  const digitalTime = document.getElementById("digitalTime");
+  const dateTime = document.getElementById("dateTime");
+  const meridiemTime = hour <= 12 ? "AM" : "PM";
+  const dateDigitalTime = `${hour}:${
+    minutes <= 9 ? "0" + minutes : minutes
+  } ${meridiemTime} `;
+  const dateTimeBody = `${thisWeekDay}, ${thisMonth} ${day}`;
+  const hourEl = document.querySelector(".hour");
+  const minuteEl = document.querySelector(".minute");
+  const secondEl = document.querySelector(".second");
 
-digitalTime.innerText = dateDigitalTime;
-dateTime.innerText = dateTimeBody;
+  digitalTime.innerText = dateDigitalTime;
+  dateTime.innerText = dateTimeBody;
+
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
+  hourEl.style.transform = `rotate(${scale(hour, 0, 12, 0, 360)}deg)`;
+
+  minuteEl.style.transform = ` rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+
+  secondEl.style.transform = `rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+}, 1000);
