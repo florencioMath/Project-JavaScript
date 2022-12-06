2411cc91af5b69a3402a7a9e8912e80376b2f130
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-06 18:54:45 -0300

    feat: Finalização da função de pegar dados do Github e função para criar um card com os dados do github.

--- challenge01/sticky-navigation/script.js
+++ challenge01/sticky-navigation/script.js
@@ -1,7 +1,19 @@
+const devNameEl = document.getElementById("dev-name");
+const devBioEl = document.getElementById("dev-bio");
+const devImgEl = document.getElementById("img-dev");
+const devLoginEl = document.getElementById("login-dev");
+const navLinks = document.querySelectorAll("nav-links");
+
 async function getDataGithub() {
   let response = await fetch("https://api.github.com/users/florencioMath");
   let data = await response.json();
-  return data;
+  creatGithubCard(data);
 }
-
 getDataGithub();
+
+function creatGithubCard(data) {
+  devNameEl.innerHTML = data.name;
+  devBioEl.innerHTML = data.bio;
+  devImgEl.src = data.avatar_url;
+  devLoginEl.innerHTML = `@${data.login}`;
+}
