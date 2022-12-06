945826169a73663e7dea791168f261db62ebaaa6
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-06 18:55:25 -0300

    feat: Implementação parcial da função de modificar a nav conforme do scroll.

--- challenge01/sticky-navigation/script.js
+++ challenge01/sticky-navigation/script.js
@@ -17,3 +17,18 @@ function creatGithubCard(data) {
   devImgEl.src = data.avatar_url;
   devLoginEl.innerHTML = `@${data.login}`;
 }
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", fixHeader);
+
+function fixHeader() {
+  if (window.scrollY > header.offsetHeight + 150) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+
+  if (window.scrollY > 934) {
+    navLinks.forEach((navLink) => (navLink.display = none));
+  }
+}
