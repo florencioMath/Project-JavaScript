76049988eab29abd70e3ae57af134052e99d6f1f
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-12-13 18:38:06 -0300

    feat: Conclusão do challenge Toast Notification.
    Implementado nomes de uma array e types aleatórios.

--- challenge01/toast-notification/script.js
+++ challenge01/toast-notification/script.js
@@ -3,16 +3,41 @@ const toast = document.getElementById("toast");
 const container = document.querySelector(".container");
 const toasts = document.getElementById("toasts");
 
+let activeMessage = 0;
+
 btnNotification.addEventListener("click", createToast);
+
+const toastMessages = [
+  "First notification",
+  "Second notification",
+  "Third notification",
+  "Fourth notification",
+];
+
+const toastTypes = ["info", "success", "error"];
 
 function createToast() {
   const toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerHTML = "Mensage here";
+  toast.classList.add(getMessageType());
+  toast.innerHTML = getMessageName();
   toast.style.display = "flex";
   toasts.appendChild(toast);
 
   setTimeout(() => {
     toast.remove();
   }, 3000);
+}
+
+function getMessageName() {
+  activeMessage++;
+
+  if (activeMessage > toastMessages.length - 1) {
+    activeMessage = 0;
+  }
+  return toastMessages[activeMessage];
+}
+
+function getMessageType() {
+  return toastTypes[Math.floor(Math.random() * toastTypes.length)];
 }
